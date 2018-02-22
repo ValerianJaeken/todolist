d361a3ab396743f5ff0197230e9d54d4fc06c196
ValerianJaeken <valerianrj@gmail.com>
2018-02-22 10:04:37 +0100

    css et html
    basiques

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ToDoList</title>
+  </head>
+  <body>
+    <style media="screen">
+      .cancerBox{
+        display: flex;
+      }
+      .cancerBox input{
+        margin-left:15px;
+      }
+      .container{
+        margin-left:35%;
+
+      }
+    </style>
+    <div class="container">
+
+
+      <h1>Ajouter une tâche</h1>
+      <br>
+      <br>
+      <p>La tâche à effectuer</p>
+      <div class="cancerBox">
+        <textarea rows="1" cols="30">
+
+        </textarea>
+        <input type="submit" value="Ajouter">
+      </div>
+    </div>
+  </body>
+</html>
